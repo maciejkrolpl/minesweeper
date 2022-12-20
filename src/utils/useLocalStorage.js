ef01b44e56fba026041cf69c670b4bfcc603d37c
Maciej Król <maciej_krol@outlook.com>
2022-12-20 08:21:51 +0100

    allow console.error in eslint

--- src/utils/useLocalStorage.js
+++ src/utils/useLocalStorage.js
@@ -20,7 +20,7 @@ function useLocalStorage(key, initialValue) {
             return item ? JSON.parse(item) : initialValue;
         } catch (error) {
             // If error also return initialValue
-            console.log(error);
+            console.error(error);
             return initialValue;
         }
     });
@@ -39,7 +39,7 @@ function useLocalStorage(key, initialValue) {
             }
         } catch (error) {
             // A more advanced implementation would handle the error case
-            console.log(error);
+            console.error(error);
         }
     };
     return [storedValue, setValue];
